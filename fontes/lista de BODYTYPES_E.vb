860f92c8d2e49f836b63a38308ae897e4c61d403
Batleman07 <alessandromarques@univille.br>
2022-10-21 16:51:14 -0300

    Trabalhando com verificações da peça

---
+++ fontes/lista de BODYTYPES_E.vb
@@ -0,0 +1,10 @@
+Dim instance As swBodyType_e
+
+
+swAllBodies 	-1 = All solid and sheet bodies
+swEmptyBody 	 5 = NULL body
+swGeneralBody	 4 = General, nonmanifold body
+swMinimumBody	 3 = Point body
+swSheetBody		 1 = Sheet body
+swSolidBody		 0 = Solid body
+swWireBody		 2 = Wire body
